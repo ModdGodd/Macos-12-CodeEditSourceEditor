6318163a8e709da1be5aa12ddc49167431aa1177
ModdGodd <98932563+ModdGodd@users.noreply.github.com>
2025-06-14 02:06:44 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -30,7 +30,6 @@ let package = Package(
         .target(
             name: "CodeEditTextView",
             dependencies: [
-                "STTextView",
                 "CodeEditLanguages",
                 "TextFormation"
             ],
